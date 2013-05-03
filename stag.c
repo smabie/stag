68b8d158f750cf8ea6a47abcdf103c829364c368
Sturm Mabie <sturm@cryptm.org>
2013-05-02 20:21:58 -0400

    refactor ncurses windows and stuff into some structs

--- stag.c
+++ stag.c
@@ -15,10 +15,20 @@
 	if (many)			        \
 		set_marked_##t (s);		\
 	else	 				\
-		set_##t (ENTRY(file_menu), s);	\
+		set_##t (ENTRY(file.menu), s);	\
 	} while (0)					
 
-WINDOW *dir_win, *file_win, *info_win, *edit_win;
+struct {
+        WINDOW  *win;
+        MENU    *menu;
+        
+} dir, file, info;
+
+struct {
+        WINDOW  *win;
+        FORM    *form;
+} edit;
+
 int resizep = 0;
 
 /* 
@@ -34,9 +44,7 @@ main(int argc, char **argv)
 	struct winsize w;
 	regex_t reg;
 	ITEM **items, *item;
-	MENU *dir_menu, *file_menu, *info_menu;
 	FIELD *edit_field[2];
-	FORM *edit_form;
 	struct entry *p;
 	char curdir[PATH_MAX], hostname[MAXHOSTNAMELEN], *s;
 	int c, hidden, tmp, many, d, regexp, dir_idx, file_idx, info_idx, idx;
@@ -47,7 +55,7 @@ main(int argc, char **argv)
 	idx = dir_idx = file_idx = info_idx = regexp = many = hidden = 0;
 	metap = 0;
 	state = DIR_MODE;
-	info_menu = NULL;
+	info.menu = NULL;
 	p = NULL;
 
 	signal(SIGWINCH, resize);
@@ -91,102 +99,102 @@ resize:
 	mvprintw(LINES - INFO_LEN + 6, 0, "year:\t ");
 	mvprintw(LINES - INFO_LEN + 7, 0, "comment:\t ");
 
-	dir_win = newwin(LINES - INFO_LEN - 1, COLS / 2, 1, 0);
-	file_win = newwin(LINES - INFO_LEN - 1, COLS / 2, 1, COLS / 2 + 1);
-	info_win = newwin(INFO_LEN - 1, COLS, LINES - INFO_LEN + 1, 9);
-	edit_win = newwin(1, COLS, LINES - 1, 0);
+	dir.win = newwin(LINES - INFO_LEN - 1, COLS / 2, 1, 0);
+	file.win = newwin(LINES - INFO_LEN - 1, COLS / 2, 1, COLS / 2 + 1);
+	info.win = newwin(INFO_LEN - 1, COLS, LINES - INFO_LEN + 1, 9);
+	edit.win = newwin(1, COLS, LINES - 1, 0);
 
-	keypad(dir_win, TRUE);
-	keypad(file_win, TRUE);
-	keypad(info_win, TRUE);
-	keypad(edit_win, TRUE);
+	keypad(dir.win, TRUE);
+	keypad(file.win, TRUE);
+	keypad(info.win, TRUE);
+	keypad(edit.win, TRUE);
 
 	intrflush(stdscr, FALSE);
-	intrflush(dir_win, FALSE);
-	intrflush(file_win, FALSE);
- 	intrflush(info_win, FALSE);
-	intrflush(edit_win, FALSE);
+	intrflush(dir.win, FALSE);
+	intrflush(file.win, FALSE);
+ 	intrflush(info.win, FALSE);
+ 	intrflush(edit.win, FALSE);
 
 	meta(stdscr, TRUE);
-	meta(dir_win, TRUE);
-	meta(file_win, TRUE);
-	meta(info_win, TRUE);
-	meta(edit_win, TRUE);
+	meta(dir.win, TRUE);
+	meta(file.win, TRUE);
+	meta(info.win, TRUE);
+	meta(edit.win, TRUE);
 
 	if (getcwd(curdir, PATH_MAX) == NULL)
 		err(1, "getcwd");
 
 	set_menu_format(NULL, LINES - INFO_LEN - 1, 0);
 
-	dir_menu = new_menu(path_make_items(curdir, hidden));
-	file_menu = new_menu(NULL);
-	info_menu = new_menu(NULL);
+	dir.menu = new_menu(path_make_items(curdir, hidden));
+	file.menu = new_menu(NULL);
+	info.menu = new_menu(NULL);
 
-	set_menu_win(dir_menu, dir_win);
-	set_menu_sub(dir_menu, dir_win);
-	set_menu_win(file_menu, file_win);
-	set_menu_sub(file_menu, file_win);
-	set_menu_win(info_menu, info_win);
-	set_menu_sub(info_menu, info_win);
+	set_menu_win(dir.menu, dir.win);
+	set_menu_sub(dir.menu, dir.win);
+	set_menu_win(file.menu, file.win);
+	set_menu_sub(file.menu, file.win);
+	set_menu_win(info.menu, info.win);
+	set_menu_sub(info.menu, info.win);
 
-	set_menu_mark(dir_menu, " ");
-	set_menu_mark(file_menu, " ");
-	set_menu_mark(info_menu, " ");
+	set_menu_mark(dir.menu, " ");
+	set_menu_mark(file.menu, " ");
+	set_menu_mark(info.menu, " ");
 
-	menu_opts_off(file_menu, O_ONEVALUE);
+	menu_opts_off(file.menu, O_ONEVALUE);
 
 	edit_field[0] = new_field(1, COLS, 0, 0, 0, 0);
 	edit_field[1] = NULL;
 	field_opts_off(edit_field[0], O_AUTOSKIP | O_STATIC);
-	edit_form = new_form(edit_field);
-	set_form_win(edit_form, edit_win);
-	set_form_sub(edit_form, edit_win);
+	edit.form = new_form(edit_field);
+	set_form_win(edit.form, edit.win);
+	set_form_sub(edit.form, edit.win);
 	
-	set_menu_items(file_menu, list_make_items());
-	post_menu(file_menu);
- 	post_menu(dir_menu);
+	set_menu_items(file.menu, list_make_items());
+	post_menu(file.menu);
+ 	post_menu(dir.menu);
 
-	nth_item(dir_menu, dir_idx);
-	nth_item(file_menu, file_idx);
+	nth_item(dir.menu, dir_idx);
+	nth_item(file.menu, file_idx);
 
 	if (state == INFO_MODE) {
-		set_menu_items(info_menu, 
-			       info_make_items(ENTRY(file_menu), 0));
-		post_menu(info_menu);		
-		nth_item(info_menu, info_idx);
+		set_menu_items(info.menu, 
+			       info_make_items(ENTRY(file.menu), 0));
+		post_menu(info.menu);		
+		nth_item(info.menu, info_idx);
 	}
 
 	refresh();
-	wrefresh(info_win);
-	wrefresh(file_win);
-	wrefresh(dir_win);
+	wrefresh(info.win);
+	wrefresh(file.win);
+	wrefresh(dir.win);
 
 	while ((c = wgetch(state == DIR_MODE ? 
-			   dir_win : (state == FILE_MODE ? 
-				      file_win : info_win))) != EOF) {
+			   dir.win : (state == FILE_MODE ? 
+				      file.win : info.win))) != EOF) {
 		if (c == 'q' && state != EDIT_MODE)
 			break;
 		if (resizep) {
 			dir_idx = item_index(current_item
-					     ((const MENU *)dir_menu));
+					     ((const MENU *)dir.menu));
 			file_idx = item_index(current_item
-					      ((const MENU *)file_menu));
+					      ((const MENU *)file.menu));
 			info_idx = item_index(current_item
-					      ((const MENU *)info_menu));
+					      ((const MENU *)info.menu));
 
 			resizep = 0;
 			ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 			resize_term(w.ws_row, w.ws_col);
 			
 			erase();
-			werase(file_win);
-			werase(dir_win);
-			werase(info_win);
+			werase(file.win);
+			werase(dir.win);
+			werase(info.win);
 			
-			delwin(dir_win);
-			delwin(file_win);
-			delwin(info_win);
-			delwin(edit_win);
+			delwin(dir.win);
+			delwin(file.win);
+			delwin(info.win);
+			delwin(edit.win);
 			endwin();
 			
 			goto resize;
@@ -199,7 +207,7 @@ resize:
 			curdir[tmp] = '/';
 			curdir[tmp + 1] = '\0';
 
-			item = current_item((const MENU *)dir_menu);
+			item = current_item((const MENU *)dir.menu);
 
 			if (strlcat(curdir, item_name(item), PATH_MAX)
 			    >= PATH_MAX)
@@ -210,23 +218,23 @@ resize:
 
 			curdir[tmp] = '\0';
 
-			unpost_menu(file_menu);			
-			items = menu_items(file_menu);
-			set_menu_items(file_menu, list_make_items());
+			unpost_menu(file.menu);			
+			items = menu_items(file.menu);
+			set_menu_items(file.menu, list_make_items());
 
 			if (items != NULL) {
 				free_items(items);
 				free(items);
 			}
 
-			post_menu(file_menu);
+			post_menu(file.menu);
  
-			wrefresh(info_win);
-			wrefresh(file_win);
+			wrefresh(info.win);
+			wrefresh(file.win);
 			refresh();
 			break;
 		case 13:	/* LF: step into directory */
-			item = current_item((const MENU *)dir_menu);
+			item = current_item((const MENU *)dir.menu);
 			
 			if (strcmp(item_name(item), "..") == 0) {
 				if ((s = strrchr(curdir, '/')) == curdir)
@@ -244,14 +252,14 @@ resize:
 					goto longer;
 			}
 
-			unpost_menu(dir_menu);
-			items = menu_items(dir_menu);
-			set_menu_items(dir_menu, 
+			unpost_menu(dir.menu);
+			items = menu_items(dir.menu);
+			set_menu_items(dir.menu, 
 				       path_make_items(curdir, hidden));
 			free_item_strings(items);
 			free_items(items);
 			free(items);
-			post_menu(dir_menu);
+			post_menu(dir.menu);
 			break;
 		longer:
 			stag_warnx("PATH_MAX of %d violated", PATH_MAX);
@@ -259,125 +267,125 @@ resize:
 			break;
 
 		case 'h':	/* toggle hidden directories */
-			unpost_menu(dir_menu);
-			items = menu_items(dir_menu);
-			set_menu_items(dir_menu, 
+			unpost_menu(dir.menu);
+			items = menu_items(dir.menu);
+			set_menu_items(dir.menu, 
 				       path_make_items(curdir, 
 						       hidden = !hidden));
 			free_item_strings(items);
 			free_items(items);
 			free(items);
-			post_menu(dir_menu);
+			post_menu(dir.menu);
 			break;
 		case KEY_DOWN:
 		case 'n':
-			menu_driver(dir_menu, REQ_DOWN_ITEM);
+			menu_driver(dir.menu, REQ_DOWN_ITEM);
 			break;
 		case KEY_NPAGE:
 		case 14:	/* C-n */
-			menu_driver(dir_menu, REQ_SCR_DPAGE);
+			menu_driver(dir.menu, REQ_SCR_DPAGE);
 			break;
 		case KEY_RIGHT:	/* switch focus to active file list */
 		case 'o':
-			if (item_count(file_menu) > 0)
+			if (item_count(file.menu) > 0)
 				state = FILE_MODE;
 			continue;
 		case KEY_UP:
 		case 'p':
-			menu_driver(dir_menu, REQ_UP_ITEM);
+			menu_driver(dir.menu, REQ_UP_ITEM);
 			break;
 		case KEY_PPAGE:
 		case 16:	/* C-p */
-			menu_driver(dir_menu, REQ_SCR_UPAGE);
+			menu_driver(dir.menu, REQ_SCR_UPAGE);
 			break;
 		}
 
 		if (state == FILE_MODE)
 		switch (c) {
 		case ' ':	/* toggle mark */
-			ENTRY(file_menu)->mark = !ENTRY(file_menu)->mark;
-			menu_driver(file_menu, REQ_TOGGLE_ITEM);
+			ENTRY(file.menu)->mark = !ENTRY(file.menu)->mark;
+			menu_driver(file.menu, REQ_TOGGLE_ITEM);
 			break;
 		case 13:	/* LF: single edit */
-			set_menu_items(info_menu, 
-				       info_make_items(ENTRY(file_menu), 0));
-			post_menu(info_menu);
+			set_menu_items(info.menu, 
+				       info_make_items(ENTRY(file.menu), 0));
+			post_menu(info.menu);
 
 			many = 0;
 			state = INFO_MODE;
 			continue;
 		case '/':	/* regex select */
 			regexp = 1;
-			post_form(edit_form);
-			wrefresh(edit_win);
+			post_form(edit.form);
+			wrefresh(edit.win);
 			state = EDIT_MODE;
 			continue;
 		rjmp:
 			if (regcomp(&reg, s, REG_EXTENDED | 
 				    REG_ICASE | REG_NOSUB) != 0)
 				break;
-			menu_driver(file_menu, REQ_FIRST_ITEM);
-			for (d = 0; d < item_count(file_menu); d++, 
-				     menu_driver(file_menu, REQ_DOWN_ITEM)) {
+			menu_driver(file.menu, REQ_FIRST_ITEM);
+			for (d = 0; d < item_count(file.menu); d++, 
+				     menu_driver(file.menu, REQ_DOWN_ITEM)) {
 				if (regexec(&reg, make_regex_str
-					    (ENTRY(file_menu)->tagp), 
+					    (ENTRY(file.menu)->tagp), 
 					    0, NULL, 0)
 				    == 0) {
-					ENTRY(file_menu)->mark = 
-						!ENTRY(file_menu)->mark;
-					menu_driver(file_menu, REQ_TOGGLE_ITEM);
+					ENTRY(file.menu)->mark = 
+						!ENTRY(file.menu)->mark;
+					menu_driver(file.menu, REQ_TOGGLE_ITEM);
 				}
 			}
-			menu_driver(file_menu, REQ_FIRST_ITEM);
+			menu_driver(file.menu, REQ_FIRST_ITEM);
 			break;
 		case 'a':	/* toggle all */
-			idx = item_index(current_item((const MENU *)file_menu));
-			menu_driver(file_menu, REQ_FIRST_ITEM);
-			for (d = 0; d < item_count(file_menu); d++, 
-				     menu_driver(file_menu, REQ_DOWN_ITEM)) {
-				ENTRY(file_menu)->mark = 
-					!ENTRY(file_menu)->mark;
-				menu_driver(file_menu, REQ_TOGGLE_ITEM);
+			idx = item_index(current_item((const MENU *)file.menu));
+			menu_driver(file.menu, REQ_FIRST_ITEM);
+			for (d = 0; d < item_count(file.menu); d++, 
+				     menu_driver(file.menu, REQ_DOWN_ITEM)) {
+				ENTRY(file.menu)->mark = 
+					!ENTRY(file.menu)->mark;
+				menu_driver(file.menu, REQ_TOGGLE_ITEM);
 			}
-			nth_item(file_menu, idx);
+			nth_item(file.menu, idx);
 			break;
 		case 'c':	/* clear */
-			unpost_menu(file_menu);
+			unpost_menu(file.menu);
 
-			items = menu_items(file_menu);
+			items = menu_items(file.menu);
 			clear_active();
-			set_menu_items(file_menu, NULL);
+			set_menu_items(file.menu, NULL);
 			if (items != NULL) {
 				free_items(items);
 				free(items);
 			}
 
-			post_menu(file_menu);
-			wrefresh(file_win);
+			post_menu(file.menu);
+			wrefresh(file.win);
 			state = DIR_MODE;
 			continue;
 		case 'd':	/* remove selected from active list */
-			unpost_menu(file_menu);
+			unpost_menu(file.menu);
 			
-			s = ENTRY(file_menu)->name;
-			idx = item_index(current_item((const MENU *)file_menu));
-			items = menu_items(file_menu);
+			s = ENTRY(file.menu)->name;
+			idx = item_index(current_item((const MENU *)file.menu));
+			items = menu_items(file.menu);
 			remove_marked();
-			set_menu_items(file_menu, list_make_items());
+			set_menu_items(file.menu, list_make_items());
 			if (items != NULL) {
 				free_items(items);
 				free(items);
 			}
 
-			post_menu(file_menu);
-			for (d = 0; d < item_count(file_menu) && 
-				     s != ENTRY(file_menu)->name; d++) {
-				menu_driver(file_menu, REQ_DOWN_ITEM);
+			post_menu(file.menu);
+			for (d = 0; d < item_count(file.menu) && 
+				     s != ENTRY(file.menu)->name; d++) {
+				menu_driver(file.menu, REQ_DOWN_ITEM);
 			}
 			/* The selected item was one of the items deleted. */
-			if (d == item_count(file_menu))
-				nth_item(file_menu, idx);
-			wrefresh(file_win);
+			if (d == item_count(file.menu))
+				nth_item(file.menu, idx);
+			wrefresh(file.win);
 			break;
 		case 'e':	/* multi edit */
 			tmp = 1;
@@ -387,9 +395,9 @@ resize:
 			}
 			if (tmp)
 				break;
-			set_menu_items(info_menu,
-				       info_make_items(ENTRY(file_menu), 1));
-			post_menu(info_menu);
+			set_menu_items(info.menu,
+				       info_make_items(ENTRY(file.menu), 1));
+			post_menu(info.menu);
 			many = 1;
 			state = INFO_MODE;
 			continue;
@@ -400,54 +408,54 @@ resize:
 
 		case KEY_DOWN:
 		case 'n':
-			menu_driver(file_menu, REQ_DOWN_ITEM);
-			draw_info(ENTRY(file_menu), info_win);
-			wrefresh(info_win);
+			menu_driver(file.menu, REQ_DOWN_ITEM);
+			draw_info(ENTRY(file.menu), info.win);
+			wrefresh(info.win);
 			break;
 		case KEY_NPAGE:
 		case 14:	/* C-n */
-			menu_driver(file_menu, REQ_SCR_DPAGE);
-			draw_info(ENTRY(file_menu), info_win);
-			wrefresh(info_win);
+			menu_driver(file.menu, REQ_SCR_DPAGE);
+			draw_info(ENTRY(file.menu), info.win);
+			wrefresh(info.win);
 			break;
 		case KEY_UP:
 		case 'p':
-			menu_driver(file_menu, REQ_UP_ITEM);
-			draw_info(ENTRY(file_menu), info_win);
-			wrefresh(info_win);
+			menu_driver(file.menu, REQ_UP_ITEM);
+			draw_info(ENTRY(file.menu), info.win);
+			wrefresh(info.win);
 			break;
 		case KEY_PPAGE:
 		case 16:	/* C-p */
-			menu_driver(file_menu, REQ_SCR_UPAGE);
-			draw_info(ENTRY(file_menu), info_win);
-			wrefresh(info_win);
+			menu_driver(file.menu, REQ_SCR_UPAGE);
+			draw_info(ENTRY(file.menu), info.win);
+			wrefresh(info.win);
 			break;
 		case 'r':	/* reload marked */
 			revert_marked();
-			draw_info(ENTRY(file_menu), info_win);
-			wrefresh(info_win);
+			draw_info(ENTRY(file.menu), info.win);
+			wrefresh(info.win);
 			break;
 		case 's':	/* write file */
-			write_entry(ENTRY(file_menu));
-			draw_info(ENTRY(file_menu), info_win);
-			wrefresh(info_win);
+			write_entry(ENTRY(file.menu));
+			draw_info(ENTRY(file.menu), info.win);
+			wrefresh(info.win);
 			break;
 		case 'u':	/* unmark all */
-			idx = item_index(current_item((const MENU *)file_menu));
-			menu_driver(file_menu, REQ_FIRST_ITEM);
-			for (d = 0; d < item_count(file_menu); d++, 
-				     menu_driver(file_menu, REQ_DOWN_ITEM)) {
-				if (ENTRY(file_menu)->mark) {
-					menu_driver(file_menu, REQ_TOGGLE_ITEM);
-					ENTRY(file_menu)->mark = 0;
+			idx = item_index(current_item((const MENU *)file.menu));
+			menu_driver(file.menu, REQ_FIRST_ITEM);
+			for (d = 0; d < item_count(file.menu); d++, 
+				     menu_driver(file.menu, REQ_DOWN_ITEM)) {
+				if (ENTRY(file.menu)->mark) {
+					menu_driver(file.menu, REQ_TOGGLE_ITEM);
+					ENTRY(file.menu)->mark = 0;
 				}
 			}
-			nth_item(file_menu, idx);
+			nth_item(file.menu, idx);
 			break;
 		case 'w':	/* write marked */
 			write_marked();
-			draw_info(ENTRY(file_menu), info_win);
-			wrefresh(info_win);
+			draw_info(ENTRY(file.menu), info.win);
+			wrefresh(info.win);
 			break;
 		}
 
@@ -455,52 +463,53 @@ resize:
 		switch (c) {
 		case 13:	/* LF: edit entry */
 			set_field_buffer(edit_field[0], 0, 
-					 item_name(current_item(info_menu)));
-			post_form(edit_form);
-			form_driver(edit_form, REQ_END_FIELD);
+					 item_name(current_item(info.menu)));
+			post_form(edit.form);
+			form_driver(edit.form, REQ_END_FIELD);
 
 			/*
 			 * XXX this doesn't make any sense ncurses is fucked up.
 			 */
-			form_driver(edit_form, ' ');
-			form_driver(edit_form, REQ_DEL_PREV);
+			form_driver(edit.form, ' ');
+			form_driver(edit.form, REQ_DEL_PREV);
 
-			wrefresh(edit_win);
+			wrefresh(edit.win);
 			state = EDIT_MODE;
 			continue;
 		case KEY_RIGHT:
 		case 'o':	/* switch focus to file list */
-			unpost_menu(info_menu);
-			free_items(menu_items(info_menu));
-			draw_info(ENTRY(file_menu), info_win);
+			unpost_menu(info.menu);
+			free_items(menu_items(info.menu));
+			draw_info(ENTRY(file.menu), info.win);
 
-			wrefresh(info_win);
+			wrefresh(info.win);
 			state = FILE_MODE;
 			continue;
 		case KEY_LEFT:
-			unpost_menu(info_menu);
-			free_items(menu_items(info_menu));
-			draw_info(ENTRY(file_menu), info_win);
+			unpost_menu(info.menu);
+			free_items(menu_items(info.menu));
+			draw_info(ENTRY(file.menu), info.win);
 
-			wrefresh(info_win);
+			wrefresh(info.win);
 			state = DIR_MODE;
 			continue;
 		case KEY_DOWN:
 		case 'n':
-			menu_driver(info_menu, REQ_DOWN_ITEM);
+			menu_driver(info.menu, REQ_DOWN_ITEM);
 			break;
 		case KEY_UP:
 		case 'p':
- 			menu_driver(info_menu, REQ_UP_ITEM);
+ 			menu_driver(info.menu, REQ_UP_ITEM);
 			break;
 		}
 
 		if (state == EDIT_MODE) 
 		switch (c) {
 		case 13:	/* LF */
-			form_driver(edit_form, REQ_NEXT_FIELD);
+			form_driver(edit.form, REQ_NEXT_FIELD);
 
 			s = str_cleanup(field_buffer(edit_field[0], 0));
+
 			set_field_buffer(edit_field[0], 0, "");
 
 			if (regexp) {
@@ -512,7 +521,7 @@ resize:
 			 * This whole scheme is pretty fucking stupid and should
 			 * probably be fixed to use function pointers.
 			 */
-			switch (item_index(current_item(info_menu))) {
+			switch (item_index(current_item(info.menu))) {
 			case 0:	CHOOSE(track, atoi(s));		break;
 			case 1:	CHOOSE(title, s);		break;
 			case 2:	CHOOSE(artist, s);		break;
@@ -521,39 +530,39 @@ resize:
 			case 5:	CHOOSE(year, atoi(s));		break;
 			case 6:	CHOOSE(comment, s);		break;
 			}
-			idx = item_index(current_item((const MENU *)info_menu));
+			idx = item_index(current_item((const MENU *)info.menu));
 
-			unpost_menu(info_menu);
-			set_menu_items(info_menu,
-				       info_make_items(ENTRY(file_menu), many));
-			post_menu(info_menu);
-			nth_item(info_menu, idx);
+			unpost_menu(info.menu);
+			set_menu_items(info.menu,
+				       info_make_items(ENTRY(file.menu), many));
+			post_menu(info.menu);
+			nth_item(info.menu, idx);
 
-			wrefresh(edit_win);
+			wrefresh(edit.win);
 			state = INFO_MODE;
 			continue;
 		case 127:	/* backspace */
-			form_driver(edit_form, REQ_DEL_PREV);
+			form_driver(edit.form, REQ_DEL_PREV);
 			break;
 		case 1:		/* C-a */
-			form_driver(edit_form, REQ_BEG_FIELD);
+			form_driver(edit.form, REQ_BEG_FIELD);
 			break;
 		case KEY_LEFT:
 		case 2:		/* C-b */
-			form_driver(edit_form, REQ_PREV_CHAR);
+			form_driver(edit.form, REQ_PREV_CHAR);
 			break;
 		case 4:		/* C-d */
-			form_driver(edit_form, REQ_DEL_CHAR);
+			form_driver(edit.form, REQ_DEL_CHAR);
 			break;
 		case 5:		/* C-e */
-			form_driver(edit_form, REQ_END_FIELD);
+			form_driver(edit.form, REQ_END_FIELD);
 			break;
 		case KEY_RIGHT:
 		case 6:		/* C-f */
-			form_driver(edit_form, REQ_NEXT_CHAR);
+			form_driver(edit.form, REQ_NEXT_CHAR);
 			break;
 		case 11:	/* C-k */
-			form_driver(edit_form, REQ_CLR_EOF);
+			form_driver(edit.form, REQ_CLR_EOF);
 			break;
 		case 27:	/* meta key */
 			metap = 1;
@@ -561,28 +570,28 @@ resize:
 		case 'b':	/* M-b */
 			if (metap) {
 				metap = 0;
-				form_driver(edit_form, REQ_PREV_WORD);
+				form_driver(edit.form, REQ_PREV_WORD);
 				break;
 			}
 		case 'f':	/* M-f */
 			if (metap) {
 				metap = 0;
-				form_driver(edit_form, REQ_NEXT_WORD);
+				form_driver(edit.form, REQ_NEXT_WORD);
 				break;
 			}
 		default:
-			form_driver(edit_form, c);
+			form_driver(edit.form, c);
 			break;
 		}
 
 		if (state == EDIT_MODE) 
-			wrefresh(edit_win);
+			wrefresh(edit.win);
 	}	
 
-	delwin(dir_win);
-	delwin(file_win);
-	delwin(info_win);
-	delwin(edit_win);
+	delwin(dir.win);
+	delwin(file.win);
+	delwin(info.win);
+	delwin(edit.win);
 	endwin();	
 
 	return 0;
